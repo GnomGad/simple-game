1ce569f9d51d918439d362daa96617959529a0e2
Zhenya <Canimore@yandex.com>
2022-10-12 00:09:54 +0300

    Updates

--- src/js/SmokeExplosion.js
+++
@@ -1,9 +0,0 @@
-const Explosion = require('./Explosion');
-class SmokeExplosion extends Explosion{
-    constructor(game,x,y){
-        super(game,x,y);
-        this.image = document.getElementById('smoke');
-    }
-}
-
-module.exports = SmokeExplosion;
